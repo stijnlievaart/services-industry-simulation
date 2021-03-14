dc35c23fc0f0a383aa371d77ae9f73fbcbb80432
Steven Hoekerd <s.a.hoekerd@students.uu.nl>
2021-03-14 14:22:12 +0100

    Added Table & Virus

--- Services Industry Simulation/Services Industry Simulation/Simulation/Table.cs
+++ Services Industry Simulation/Services Industry Simulation/Simulation/Table.cs
@@ -1,16 +1,34 @@
-﻿namespace Services_Industry_Simulation.Simulation
+﻿using System.Collections.Generic;
+namespace Services_Industry_Simulation.Simulation
 {
     public class Table
     {
-        Seat[] seats;
-        Group group;
-        FPoint location;
-        FPoint size;
-        public Table(Seat[] seats, FPoint location, FPoint size)
+        
+        public Group activeGroup;
+        readonly public Seat[] seats;
+        readonly public float onRouteLocation;
+        readonly public Route onRoute;
+        public List<Group> pastGroups;
+        readonly public FPoint location;
+        readonly public FPoint size;
+
+        public Table(Seat[] seats, float onRouteLocation, Route onRoute, FPoint location, FPoint size)
         {
             this.seats = seats;
             this.location = location;
             this.size = size;
+            this.onRouteLocation = onRouteLocation;
+            this.onRoute = onRoute;
+            this.pastGroups = new List<Group>();
+        }
+
+        public void SetGroup(Group group)
+        {
+            if (activeGroup != null)
+            {
+                pastGroups.Add(activeGroup);
+            }
+            activeGroup = group;
         }
     }
 }
--- Services Industry Simulation/Services Industry Simulation/Simulation/Virus.cs
+++ Services Industry Simulation/Services Industry Simulation/Simulation/Virus.cs
@@ -1,10 +1,27 @@
-﻿namespace Services_Industry_Simulation.Simulation
+﻿using System.Collections.Generic;
+namespace Services_Industry_Simulation.Simulation
 {
     class Virus
     {
-        public Virus()
+        public List<Person> virusGivers;
+        public List<Person> virusTakers;
+
+        public Virus( List<Person> virusGivers )
         {
+            this.virusGivers = virusGivers;
+            this.virusTakers = new List<Person>();
 
         }
+
+        public void AddGiver(Person giver)
+        {
+            virusGivers.Add(giver);
+        }
+
+        public void AddTaker(Person taker)
+        {
+            virusTakers.Add(taker);
+        }
+
     }
 }
